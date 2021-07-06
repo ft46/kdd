5b9837a8683f7eca76781968673851560cb39611
fateme tahmasbi <86785778+ft46@users.noreply.github.com>
2021-07-06 05:35:22 +0430

    Add files via upload

---
+++ a.py
@@ -0,0 +1,14 @@
+print(__name__)
+print(5)
+def aa():
+    print(11)
+if __name__== "__main__":
+    aa()
+# /////////////////////////////
+grade=[57,49,0 ,89,66]
+for i in grade:
+    assert i != 0 , "grade is zero"
+    if i>54:
+        print('pas')
+    else:
+        print('fail')
